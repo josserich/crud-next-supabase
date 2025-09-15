d6ea3ad8a9cf4561b1c5334fafad8bbfb7447cae
Josse29 <pinemjosse@gmail.com>
2025-09-15 16:20:32 +0700

    deployment

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiInstance = axios.create({
-  baseURL: `${process.env.NEXT_PUBLIC_SITE_URL}/api`,
+  baseURL: `/api`,
   //   timeout: 1000,
   //   headers: { "X-Custom-Header": "foobar" },
 });
