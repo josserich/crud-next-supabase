3d41cddc580e5fdc27466b8db532b45e5679cb3f
Josse29 <pinemjosse@gmail.com>
2025-09-15 17:16:20 +0700

    updating font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,12 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import { AuthProvider } from "@/src/contextts/context-auth";
+import { Montserrat } from "next/font/google";
+
+const montserrat = Montserrat({
+  subsets: ["latin"],
+  weight: ["700"], // array
+});
 
 export const metadata: Metadata = {
   title: "CRUD + AUTH",
@@ -10,14 +16,14 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
     <html lang="en">
-      <AuthProvider>
-        <body>{children}</body>
-      </AuthProvider>
+      <body className={montserrat.className}>
+        <AuthProvider>{children}</AuthProvider>
+      </body>
     </html>
   );
 }
