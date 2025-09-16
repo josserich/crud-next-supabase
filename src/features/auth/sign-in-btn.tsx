88c6b60cfb472af2aa4a503087d7af1f10abba14
Josse29 <pinemjosse@gmail.com>
2025-09-16 11:10:45 +0700

    feat: implement authentication callback and session handling in sign-in button

--- src/features/auth/sign-in-btn.tsx
+++ src/features/auth/sign-in-btn.tsx
@@ -1,11 +1,14 @@
 "use client";
 import { useAuth } from "@/src/contextts/context-auth";
 import authAPI from "@/src/services/auth";
-import React from "react";
+import React, { useEffect } from "react";
 import { FcGoogle } from "react-icons/fc";
+import { useRouter } from "next/navigation";
+import userAPI from "@/src/services/user";
 
 const SignInBtn = () => {
-  const { loading, setLoading } = useAuth();
+  const { loading, setLoading, setUserSignin } = useAuth();
+  const router = useRouter();
   const handleSignIn = async () => {
     setLoading(true);
     try {
@@ -17,6 +20,28 @@ const SignInBtn = () => {
       setLoading(false);
     }
   };
+  const checkSession = async () => {
+    setLoading(true);
+    try {
+      const authenticated = await authAPI.getAuthentication();
+      if (authenticated) {
+        await userAPI.createUser({
+          name: authenticated.full_name,
+          email: authenticated.email,
+        });
+        setUserSignin(authenticated);
+        router.push("/product");
+      }
+    } catch (error) {
+      console.error(error);
+      throw error;
+    } finally {
+      setLoading(false);
+    }
+  };
+  useEffect(() => {
+    checkSession();
+  }, []);
   return (
     <div className="flex justify-center">
       {loading && <span className="loading loading-dots loading-xl"></span>}
