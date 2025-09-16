8ef81f915dff56e982412b5199eea665ec322d72
Josse29 <pinemjosse@gmail.com>
2025-09-16 10:33:11 +0700

    update redirect URL for Google OAuth sign-in

--- src/services/auth.ts
+++ src/services/auth.ts
@@ -5,7 +5,7 @@ const signIn = async () => {
   await supabase.auth.signInWithOAuth({
     provider: "google",
     options: {
-      redirectTo: `${window.location.origin}/auth/callback`,
+      redirectTo: `https://crud-next-supabase-lilac.vercel.app/auth/callback`,
     },
   });
 };
